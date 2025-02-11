2ab9cd5f43f5293721894e31224869daf5d69fe8
rapexa <rapexamrs@gmail.com>
2025-02-11 06:38:48 -0800

    add entity user

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -1,1 +1,12 @@
 package entity
+
+import "time"
+
+type User struct {
+	ID        uint      `gorm:"primaryKey"`
+	Name      string    `gorm:"size:255;not null"`
+	Email     string    `gorm:"size:255;uniqueIndex;not null"`
+	Password  string    `gorm:"size:255;not null"`
+	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP"`
+	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP"`
+}
