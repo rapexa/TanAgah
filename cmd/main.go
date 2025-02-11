adc90a84e7087add587d839e7b2d00c935f38224
rapexa <rapexamrs@gmail.com>
2025-02-11 06:41:43 -0800

    main file

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"TanAgah/internal/config"
+	"TanAgah/internal/logger"
+)
+
+func main() {
+	// Load configuration
+	cfg := config.LoadConfig()
+
+	// Initialize logger
+	logger.InitLogger()
+	
+}
