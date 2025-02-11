0b2af0731424c0aeaf3801bc72bc87734b9e865e
rapexa <rapexamrs@gmail.com>
2025-02-11 06:39:38 -0800

    add logger system

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,1 +1,16 @@
 package logger
+
+import (
+	"go.uber.org/zap"
+)
+
+var Log *zap.Logger
+
+func InitLogger() {
+	var err error
+	Log, err = zap.NewProduction()
+	if err != nil {
+		panic("Failed to initialize logger")
+	}
+	defer Log.Sync()
+}
