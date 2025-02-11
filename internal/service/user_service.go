33d8fa5d977aff9c7e1b398a1de873331b717f19
rapexa <rapexamrs@gmail.com>
2025-02-11 06:43:45 -0800

    add some services

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -1,1 +1,37 @@
 package service
+
+import (
+	"TanAgah/internal/entity"
+	"TanAgah/internal/repository"
+)
+
+type UserService interface {
+	CreateUser(user *entity.User) error
+	GetUser(id uint) (*entity.User, error)
+	UpdateUser(user *entity.User) error
+	DeleteUser(id uint) error
+}
+
+type userService struct {
+	userRepo repository.UserRepository
+}
+
+func NewUserService(userRepo repository.UserRepository) UserService {
+	return &userService{userRepo}
+}
+
+func (s *userService) CreateUser(user *entity.User) error {
+	return s.userRepo.Create(user)
+}
+
+func (s *userService) GetUser(id uint) (*entity.User, error) {
+	return s.userRepo.FindByID(id)
+}
+
+func (s *userService) UpdateUser(user *entity.User) error {
+	return s.userRepo.Update(user)
+}
+
+func (s *userService) DeleteUser(id uint) error {
+	return s.userRepo.Delete(id)
+}
