e78f48cd7f54a3b65d81422b09fe2a1310c849b5
rapexa <rapexamrs@gmail.com>
2025-02-11 06:37:59 -0800

    add loadConfig part and Init db

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,39 @@
 package config
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"os"
+)
+
+type Config struct {
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	AppPort    string
+}
+
+func LoadConfig() *Config {
+	godotenv.Load()
+	return &Config{
+		DBHost:     os.Getenv("DB_HOST"),
+		DBPort:     os.Getenv("DB_PORT"),
+		DBUser:     os.Getenv("DB_USER"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+		DBName:     os.Getenv("DB_NAME"),
+		AppPort:    os.Getenv("APP_PORT"),
+	}
+}
+
+func InitDB(cfg *Config) *gorm.DB {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", cfg.DBUser, cfg.DBPassword, cfg.DBHost, cfg.DBPort, cfg.DBName)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+	return db
+}
